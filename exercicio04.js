79327611bd44d27c5b555ef7a77945a8683d9b04
Thays Peixoto <thabysilva12@gmail.com>
2025-11-28 14:25:11 -0300

    adiciona solucao exercicio04

--- exercicio04.js
+++ exercicio04.js
@@ -1,8 +1,10 @@
-let n1 = 5
-let n2 = 6
-let n3 = 7
-let n4 = 8
+const entrada = require("readline-sync")
 
-let calculo = (n1 * n2) - (n3 * n4)
+const numero1 = entrada.questionFloat("digite o primeiro numero: ")
+const numero2 = entrada.questionFloat("digite o segundo numero: ")
+const numero3 = entrada.questionFloat("digite o terceiro numero: ")
+const numero4 = entrada.questionFloat("digite o quarto numero: ")
 
-console.log(calculo.toFixed(1))
+const produto = (numero1 * numero2) - (numero3 * numero4)
+
+console.log(produto.toFixed(1))
