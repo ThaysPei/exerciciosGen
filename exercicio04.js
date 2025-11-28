b5955a55283ca6fa10c97b96d355424fa50e8c7f
Thays Peixoto <thabysilva12@gmail.com>
2025-11-28 14:29:48 -0300

    melhora o exercicio04

--- exercicio04.js
+++ exercicio04.js
@@ -1,10 +1,20 @@
 const entrada = require("readline-sync")
 
+const geraProduto = (numero1, 
+                    numero2, 
+                    numero3, 
+                    numero4, 
+                    qtdDecimais) => {
+    return ((numero1 * numero2) - (numero3 * numero4)).toFixed(qtdDecimais)
+}
+
+const pegarNumeros = () => {
+    // Pegar numeros de 1 a 4.
+}
+
 const numero1 = entrada.questionFloat("digite o primeiro numero: ")
 const numero2 = entrada.questionFloat("digite o segundo numero: ")
 const numero3 = entrada.questionFloat("digite o terceiro numero: ")
 const numero4 = entrada.questionFloat("digite o quarto numero: ")
 
-const produto = (numero1 * numero2) - (numero3 * numero4)
-
-console.log(produto.toFixed(1))
+console.log(geraProduto(numero1, numero2, numero3, numero4, 1))
