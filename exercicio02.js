a809a492ed098abc5ef0c1c23636fdcdc761983d
Thays Peixoto <thabysilva12@gmail.com>
2025-11-28 14:25:04 -0300

    adiciona solucao exercicio02

--- exercicio02.js
+++ exercicio02.js
@@ -1,8 +1,24 @@
-let nota1 = 10.0
-let nota2 = 8.0
-let nota3 = 7.0
-let nota4 = 7.5
+const entrada = require("readline-sync");
 
-let media = (nota1 + nota2 + nota3 + nota4)/4
+const funcaoMedia = (nota1, 
+                    nota2, 
+                    nota3, 
+                    nota4, 
+                    qtdDecimais) => {
+    return ((nota1 + nota2 + nota3 + nota4) / 4).toFixed(qtdDecimais)
+}
 
-console.log(media.toFixed(1))
+const nota1 = entrada.questionInt("Digite a primeira nota: ");
+console.log(`A sua nota é : ${nota1}`);
+
+const nota2 = entrada.questionInt("Digite a segunda nota: ");
+console.log(`A sua nota é : ${nota2}`);
+
+const nota3 = entrada.questionInt("Digite a terceira nota: ");
+console.log(`A sua nota é : ${nota3}`);
+
+const nota4 = entrada.questionInt("Digite a quarta nota: ");
+console.log(`A sua nota é : ${nota4}`);
+
+const media = funcaoMedia(nota1, nota2, nota3, nota4, 1)
+console.log(`A sua media é: ${media}`);
