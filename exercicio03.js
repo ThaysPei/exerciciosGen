ea7426c07abf7008fcbd4270c008a24cd99b5290
Thays Peixoto <thabysilva12@gmail.com>
2025-11-28 14:25:08 -0300

    adiciona solucao exercicio03

--- exercicio03.js
+++ exercicio03.js
@@ -1,11 +1,9 @@
-let salarioBruto = 2000.00
-let adicionalNoturno = 500.00
-let horasExtras = 100.00
-let descontos = 200.00
-let salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos
+const entrada = require("readline-sync")
+
+const salarioBruto = entrada.questionFloat("Digite o seu salario bruto")
+const adicionalNoturno = entrada.questionFloat("Digite o seu adicional noturno")
+const horasExtras = entrada.questionFloat("Digite suas horas extras")
+const descontos = entrada.questionFloat("Digite o seu desconto") 
+const salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos
 
 console.log(salarioLiquido)
-
-
-
-
