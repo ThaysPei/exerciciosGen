956c84e016b2b5015d97a8dfce404698554e0d5e
Thays Peixoto <thabysilva12@gmail.com>
2025-11-28 12:44:08 -0300

    realizei exercício 03

---
+++ exercicio03.js
@@ -0,0 +1,11 @@
+let salarioBruto = 2000.00
+let adicionalNoturno = 500.00
+let horasExtras = 100.00
+let descontos = 200.00
+let salarioLiquido = salarioBruto + adicionalNoturno + (horasExtras * 5) - descontos
+
+console.log(salarioLiquido)
+
+
+
+
