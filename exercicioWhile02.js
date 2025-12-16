e4b1da0fa1dd3a1936cd2dd9162b790f7f1d0d54
Thays Peixoto <thabysilva12@gmail.com>
2025-12-16 20:18:35 -0300

    realizada correção de exercício

--- exercicioWhile02.js
+++ exercicioWhile02.js
@@ -1,21 +1,23 @@
 const leia = require('readline-sync');
 
-let menor21 = 0;   
+let menor21 = 0;
 let maior50 = 0;
+
 let idades = leia.questionInt('Digite a idade: ');
-console.log(menor21)
 
-while(idades != 0) {
+while (idades !== 0) {
 
-    if(idades < 21){
+    if (idades < 21) {
         menor21++;
     }
-    if(idades > 50){
+
+    if (idades > 50) {
         maior50++;
     }
-    console.log(menor21)
-//    idades = leia.questionInt('Digite a idade: ');
+
+    idades = leia.questionInt('Digite a idade: ');
 }
-console.log(menor21)
+
 console.log(`Quantidade de pessoas menores de 21 anos: ${menor21}`);
 console.log(`Quantidade de pessoas maiores de 50 anos: ${maior50}`);
+
